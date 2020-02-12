f690dc436ff7c0f6243c8c2431f722de0dba37da
Ricardo Silva <rick.silva@gmail.com>
2020-02-12 11:19:27 +0000

    Add simple validations to the payment endpoint
    Implements simple validations for the payment endpoint.

--- payments/app/controllers/payments_controller.rb
+++ payments/app/controllers/payments_controller.rb
@@ -2,6 +2,20 @@ class PaymentsController < ApplicationController
   include Response
 
   def create
-    json_response({id: 0, result: true}, :created)
+    return json_response({result: false, message: 'missing parameter'}, :bad_request) if missing_param?
+    return json_response({result: false, message: 'invalid value'}, :bad_request) if !valid_value?
+
+    json_response({id: SecureRandom.hex(10), result: true}, :created)
+  end
+
+  private
+
+  def missing_param?
+    !params.key?(:currency) || !params.key?(:value) || !params.key?(:customer_id)
+  end
+
+  def valid_value?
+    value = params[:value]
+    params[:value].to_f >= 0.00
   end
 end
--- payments/spec/requests/payments_spec.rb
+++ payments/spec/requests/payments_spec.rb
@@ -21,5 +21,69 @@ RSpec.describe 'Payments API', type: :request do
         expect(json['result']).to be_truthy
       end
     end
+
+    context 'when the request is missing the currency' do
+      let(:attributes) { valid_attributes.except(:currency) }
+
+      before { post '/payments', params: attributes }
+
+      it { expect(response).to have_http_status(:bad_request) }
+
+      it 'does not create a payment' do
+        expect(json['id']).to be_nil
+      end
+
+      it 'returns result false' do
+        expect(json['result']).to be_falsey
+      end
+    end
+
+    context 'when the request is missing the value' do
+      let(:attributes) { valid_attributes.except(:value) }
+
+      before { post '/payments', params: attributes }
+
+      it { expect(response).to have_http_status(:bad_request) }
+
+      it 'does not create a payment' do
+        expect(json['id']).to be_nil
+      end
+
+      it 'returns result false' do
+        expect(json['result']).to be_falsey
+      end
+    end
+
+    context 'when the request is missing the customer_id' do
+      let(:attributes) { valid_attributes.except(:customer_id) }
+
+      before { post '/payments', params: attributes }
+
+      it { expect(response).to have_http_status(:bad_request) }
+
+      it 'does not create a payment' do
+        expect(json['id']).to be_nil
+      end
+
+      it 'returns result false' do
+        expect(json['result']).to be_falsey
+      end
+    end
+
+    context 'when the request has negative value' do
+      let(:attributes) { valid_attributes.merge(value: -11.11) }
+
+      before { post '/payments', params: attributes }
+
+      it { expect(response).to have_http_status(:bad_request) }
+
+      it 'does not create a payment' do
+        expect(json['id']).to be_nil
+      end
+
+      it 'returns result false' do
+        expect(json['result']).to be_falsey
+      end
+    end
   end
 end
